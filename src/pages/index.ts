fe1de3d9304d89ee35d06e7311b433f1acd3c4e6
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2025-09-17 20:09:30 -0500

    segundo commit

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,8 @@
 export async function GET() {
-  	return new Response(null, {
-    	status: 302,
-    	headers: {
-     		Location: "/es",
-    	},
-  	});
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "/es", // o "/en"
+    },
+  });
 }
